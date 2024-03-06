70e022139f34ee1cacca64b5d77ddd8631553cdd
jsump <libbyn79@gmail.com>
2024-03-06 23:25:21 +0000

    Task 6 edit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup() {
   return Promise.allSettled([
     uploadPhoto(fileName),
     signUpUser(firstName, lastName),
