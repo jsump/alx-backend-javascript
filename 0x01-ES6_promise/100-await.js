2197a6fe6e27c028a8cf306c21a1fd77bcfbb2fd
jsump <libbyn79@gmail.com>
2024-03-06 23:06:23 +0000

    Async function using await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
