2f727c9ee7a3ff35942494c113d625179d2ccc9b
jsump <libbyn79@gmail.com>
2024-03-06 19:58:27 +0000

    Task 3 edit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch((error) => {
-      console.error('Signup system offline', error);
+      console.log('Signup system offline', error);
     });
 }
