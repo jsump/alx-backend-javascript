9b79460772d32abf13d76faf482d19b5afa8d106
jsump <libbyn79@gmail.com>
2024-03-06 19:00:30 +0000

    Append 3 handlers to function

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a reponse from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.error(error);
+      return {};
+    });
+}
