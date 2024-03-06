914185a1cfb7a3927914e85de9115742a74cddf0
jsump <libbyn79@gmail.com>
2024-03-06 18:10:01 +0000

    Return promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,15 @@
-Promise.getResponseFromAPI = function() {
-    return new Promise((resolve, reject) => {
-    });
-};
+export default function getResponseFromAPI() {
+  Promise.getResponseFromAPI = () => new Promise((resolve, reject) => {
+    const successfulRequest = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (successfulRequest) {
+        const data = { example: 'Response data' };
+        resolve(data);
+      } else {
+        const error = new Error('Request Failed');
+        reject(error);
+      }
+    }, 1000);
+  });
+}
