ca557e669e340422b53a18a0a8eb9ddc2648d547
jsump <libbyn79@gmail.com>
2024-03-06 23:20:16 +0000

    Task 6 edit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise((resolve) => {
-    const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-    Promise.allSettled(promises).then((results) => {
-      const finalResults = results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-      resolve(finalResults);
-    });
-  });
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
