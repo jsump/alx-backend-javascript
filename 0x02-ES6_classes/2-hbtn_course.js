3af9b9b6ab7f1654f024fab1a87b6aacd92cfe84
jsump <libbyn79@gmail.com>
2024-03-07 11:50:44 +0000

    Getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = Array.isArray(students) ? students : [];
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
+    } else {
+      throw new Error('Students must be an array');
+    }
+  }
+}
