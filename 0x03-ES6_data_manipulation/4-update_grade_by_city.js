c52a441075d92239de2db81e4f9b6f93c8b06af2
jsump <libbyn79@gmail.com>
2024-03-11 21:15:44 +0000

    Task 4 edit

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,6 +3,9 @@ export default function updateStudentGradeByCity(studentDetails, city, newGrades
     .filter((student) => student.location === city)
     .map((student) => {
       const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
-      return gradeObject ? { ...student, grade: gradeObject.grade } : student;
+      return {
+        ...student,
+        grade: gradeObject ? gradeObject.grade  : 'N/A'
+      };
     });
 }
