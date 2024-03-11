1fc004b6cebaafe004983086ec5611b3d224ec79
jsump <libbyn79@gmail.com>
2024-03-11 23:20:51 +0000

    Task 8 edit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
-  const valuesAfterFilter = [...set].filter((value) => value.startsWith(startString));
+  const valuesAfterFilter = [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length));
+
   return valuesAfterFilter.join('-');
 }
