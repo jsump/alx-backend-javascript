3a9fe53b9d302a86fbd3554c42dfbf093db5495b
jsump <libbyn79@gmail.com>
2024-03-12 12:25:20 +0000

    Return updated map for all items w initial quantity of 1

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      updatedMap.set(key, 100);
+    } else {
+      updatedMap.set(key, value);
+    }
+  }
+  return updatedMap;
+}
