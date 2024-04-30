14853490eb5747e84f507487cad928ea148cd26f
jsump <libbyn79@gmail.com>
2024-04-30 23:41:15 +0000

    single quotes

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,7 +4,7 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (error, data) => {
       if (error) {
-        reject(new Error(`Cannot load the database`));
+        reject(new Error('Cannot load the database'));
       } else {
         const lines = data.split('\n');
         const headers = lines[0].split(',').map((header) => header.trim());
