29f7f952b152429ecaee33473d1bfeee3423291a
jsump <libbyn79@gmail.com>
2024-04-30 20:39:43 +0300

    Task #1 edit new line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,30 +3,35 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  // Check if input is from a TTY (terminal)
-  terminal: process.stdin.isTTY,
 });
 
 // Initial prompt display
 console.log('Welcome to Holberton School, what is your name?');
 
-// Where we listen for user input
+// Listen for user input
 rl.on('line', (input) => {
   if (input === 'exit') {
-    // WHen the user ends the prpgram
-    console.log('This important software is now closing');
+    // When the user ends the program
+    console.log('This important software is now closing\n');
     rl.close();
   } else {
     console.log(`Your name is: ${input}`);
   }
 });
 
-// When interface is closed
+// When the interface is closed
 rl.on('close', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\n');
 });
 
-// When the interface is closed + non-interactive input(piped input)/TTY
+// Handle command line arguments
+if (process.argv.length > 2) {
+  const name = process.argv[2];
+  console.log(`Your name is: ${name}\n`);
+  rl.close();
+}
+
+// Handle input from non-TTY
 if (!process.stdin.isTTY) {
   let inputData = '';
   process.stdin.setEncoding('utf-8');
@@ -37,8 +42,8 @@ if (!process.stdin.isTTY) {
   process.stdin.on('end', () => {
     console.log('Welcome to Holberton School, what is your name?');
     console.log(`Your name is: ${inputData.trim()}`);
-    console.log('This important software is now closing');
+    console.log('This important software is now closing\n');
   });
 }
 
-module.exports = rl();
+module.exports = rl;
