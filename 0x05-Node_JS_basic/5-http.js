dc586fa2d5e617c3688566143e6731e793dcf9f4
jsump <libbyn79@gmail.com>
2024-04-30 17:36:49 +0300

    Task 5 edit

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,28 +2,54 @@ const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        const database = 'database.cvs';
-        fs.readFile(database, 'utf-8', (error, data) => {
-            if (error) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('internal Server error');
-            } else {
-                const students = data.split('\n'.filter((student) => student.trim() !==''));
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end(`This is the list of our students: \n${students.join('\n')}`);
-            }
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const database = 'database.csv';
+    fs.readFile(database, 'utf-8', (error, data) => {
+      if (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        const lines = data.split('\n');
+        const headers = lines[0].split(',').map((header) => header.trim());
+        const studentsData = lines.slice(1);
+
+        const fieldIndexMap = {
+          CS: [],
+          SWE: [],
+        };
+
+        studentsData.forEach((student) => {
+          const fields = student.split(',');
+          const field = fields[headers.indexOf('field')].trim();
+          const firstName = fields[headers.indexOf('firstname')].trim();
+
+          if (field in fieldIndexMap) {
+            fieldIndexMap[field].push(firstName);
+          }
         });
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end("Not Found");
-    }
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${studentsData.length}\n`);
+
+        Object.entries(fieldIndexMap).forEach(([field, students]) => {
+          res.write(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`);
+        });
+
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
+
 app.listen(1245, () => {
-    console.log('Server is running on port 1245');
+  console.log('Server is running on port 1245');
 });
 
 module.exports = app;
