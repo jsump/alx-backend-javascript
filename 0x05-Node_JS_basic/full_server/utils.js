0442522e683453a22d07ed25f4526aaf5cac2ae4
jsump <libbyn79@gmail.com>
2024-04-30 18:31:59 +0300

    Routes and server that will reuse everything created

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,10 +1,10 @@
-import { Resolver } from "dns";
-import fs from "fs";
-import { resolve } from "path";
+import { Resolver } from 'dns';
+import fs from 'fs';
+import { resolve } from 'path';
 
 export function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf-8", (error, data) => {
+    fs.readFile(filePath, 'utf-8', (error, data) => {
       if (error) {
         reject(error);
       } else {
