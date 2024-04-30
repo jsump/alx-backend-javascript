f6f9894a6cdc21e935d68000f725c3a1ba74673e
jsump <libbyn79@gmail.com>
2024-04-30 21:01:04 +0300

    ESLint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-var app = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('content-Type', 'text/plain');
   res.end('Hello Holberton School!');
