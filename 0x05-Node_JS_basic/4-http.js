a24b83076736d088e517056e2dcdfb88ef432bbc
jsump <libbyn79@gmail.com>
2024-04-30 13:51:20 +0300

    Task #4 edit

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,16 @@
 const http = require('http');
 
 function createServer() {
-  const app = http.createServer((req, res) => {
+  var app = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('content-Type', 'text/plain');
     res.end('Hello Holberton School!');
-});
+  });
 
   app.listen(1245, () => {
     console.log('Server is running on port 1245');
   });
-  
+
   return app;
 }
 
