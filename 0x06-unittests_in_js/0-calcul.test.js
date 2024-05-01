d894ec4907376855176989915393d280c0b082dc
jsump <libbyn79@gmail.com>
2024-05-01 20:36:48 +0000

    Test 0 edit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-    it('must return the sum of two numbers, rounded to the next whole number' , () => {
+    it('must return the sum of two numbers, rounded to the next whole number', () => {
         assert.strictEqual(calculateNumber(3.6, 3.1), 7);
         assert.strictEqual(calculateNumber(2.9, 4.6), 8);
         assert.strictEqual(calculateNumber(5.6, -2.1), 4);
