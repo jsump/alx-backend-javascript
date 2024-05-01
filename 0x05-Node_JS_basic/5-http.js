5c3e7030cd7524b10a0a971e9ceb06b353d3d659
jsump <libbyn79@gmail.com>
2024-05-01 14:47:56 +0000

    Task 5 correction on error message

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,7 +10,7 @@ const app = http.createServer((req, res) => {
     fs.readFile(database, 'utf-8', (error, data) => {
       if (error) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
+        res.end('Cannot load the database');
       } else {
         const lines = data.split('\n').filter((line) => line.trim() !== '');
         const headers = lines[0].split(',').map((header) => header.trim());
