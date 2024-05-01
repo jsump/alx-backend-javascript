203cc47dd654b535c945721863947c07bfacb1cc
jsump <libbyn79@gmail.com>
2024-05-01 19:35:00 +0000

    Edit task #0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,6 @@
 function calculateNumber(a, b) {
-    const roundedA = a < 0? Math.round(a +0.5) : Math.round(a);
-    const roundedB = b < 0? Math.round(b +0.5) : Math.round(b);
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
     return roundedA + roundedB;
 }
 module.exports = calculateNumber;
