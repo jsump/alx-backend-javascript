445b42bbc9bcc8aa023abc0de8ea8e865c112a58
jsump <libbyn79@gmail.com>
2024-05-01 20:31:17 +0000

    Proper descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,23 +2,28 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
-	test('must correctly add two rounded numbers', () => {
-		const result = calculateNumber('SUM', 6.7, 4.6);
-		assert.strictEqual(result, 11);
+	describe('SUM', () => {
+		it('must correctly add two rounded numbers', () => {
+			const result = calculateNumber('SUM', 6.7, 4.6);
+			assert.strictEqual(result, 12);
+		});
 	});
 
-	test('must correctly subtract two rounded numbers', () => {
-                const result = calculateNumber('SUBTRACT', 5, 2);
-                assert.strictEqual(result, 3);
-        });
+	describe('SUBTRACT', () => {
+		it('must correctly subtract two rounded numbers', () => {
+			const result = calculateNumber('SUBTRACT', 5, 2);
+                	assert.strictEqual(result, 3);
+        	});
+	});
 
-	test('must correctly divide two rounded numbers', () => {
-                const result = calculateNumber('DIVIDE', 6, 3);
-                assert.strictEqual(result, 2);
-        });
-
-	test('must return "Error" when dividing by zero', () => {
-                const result = calculateNumber('DIVIDE', 4, 0);
-                assert.strictEqual(result, 'Error');
-        });
+	describe('DIVIDE', () => {
+		it('must correctly divide two rounded numbers', () => {
+                	const result = calculateNumber('DIVIDE', 6, 3);
+                	assert.strictEqual(result, 2);
+        	});
+		it('must return "Error" when dividing by zero', () => {
+                	const result = calculateNumber('DIVIDE', 4, 0);
+                	assert.strictEqual(result, 'Error');
+        	});
+	});
 });
