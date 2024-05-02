b9cac7e76e01004338bab70c6d747d63a50992b0
jsump <libbyn79@gmail.com>
2024-05-02 22:35:33 +0300

    Test 0 correction

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,14 +2,34 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-    it('must return the sum of two numbers, rounded to the next whole number', () => {
-        assert.strictEqual(calculateNumber(3.6, 3.1), 7);
-        assert.strictEqual(calculateNumber(2.9, 4.6), 8);
-        assert.strictEqual(calculateNumber(5.6, -2.1), 4);
-        assert.strictEqual(calculateNumber(6, 1.1), 7);
-        assert.strictEqual(calculateNumber(-2.5, 3.1), 1);
-        assert.strictEqual(calculateNumber(-3.3, -8.4), -11);
-        assert.strictEqual(calculateNumber(5.8, -5.5), 1);
-        assert.strictEqual(calculateNumber(0, 0), 0);
+    describe('sum of two positive numbers', () => {
+        it('must return the sum of two positive numbers, rounded to the next whole number', () => {
+            assert.strictEqual(calculateNumber(3.6, 3.1), 7);
+            assert.strictEqual(calculateNumber(2.9, 4.6), 8);
+            assert.strictEqual(calculateNumber(6, 1.1), 7);
+            assert.strictEqual(calculateNumber(5.6, -2.1), 4);
+            assert.strictEqual(calculateNumber(2, 4), 6);
+            assert.strictEqual(calculateNumber(1, 2), 3);
+        });
+    });
+    
+    describe('sum of a negative number and a positive number', () => {
+        it('must return the sum of a negative and a positive numbers, rounded to the next whole number', () => {
+            assert.strictEqual(calculateNumber(-2.5, 3.1), 1);
+            assert.strictEqual(calculateNumber(-3.3, 8.4), 5);
+            assert.strictEqual(calculateNumber(5.8, -5.5), 1);
+            assert.strictEqual(calculateNumber(0, 0), 0);
+            assert.strictEqual(calculateNumber(2, -3), -1);
+        });
+    });
+
+    describe('sum of two negative numbers', () => {
+        it('must return the sum of two negative numbers, rounded to the next whole number', () => {
+            assert.strictEqual(calculateNumber(-2, -3), -5);
+            assert.strictEqual(calculateNumber(-1.5, -8.4), -9);
+            assert.strictEqual(calculateNumber(-5., -2.3), -7);
+            assert.strictEqual(calculateNumber(-1, -4), -5);
+            assert.strictEqual(calculateNumber(-12, -3), -15);
+        });
     });
 });
