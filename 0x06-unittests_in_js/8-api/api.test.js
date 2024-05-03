7b683be6af28bed13cc21264c8fe01a2098019e0
jsump <libbyn79@gmail.com>
2024-05-03 17:28:56 +0300

    Add request

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -8,34 +8,27 @@ const app = require("./api.js");
 describe("API", () => {
     describe("GET /", () => {
         // Test correct status code
-        it(" must return the correct status code(OK: 200)", (done) => {
-            expectdone();
-                });
+        it("must return the correct status code(OK: 200)", (done) => {
+            request.get('http://localhost:7865', (error, respose, body) => {
+                expect(respose.statusCode).to.equal(200);
+                done();
+            });
         });
 
         // Test correct result
         it("must return the correct result", (done) => {
-            supertest(app)
-                .get("/")
-                .expect("Welcome to the payment system")
-                .end((error, res) => {
-                    if (error)
-                        return done(error);
-                    done();
-                });
+            request.get('http://localhost:7865', (error, respose, body) => {
+                expect(respose.statusCode).to.equal("Welcome to the paymnet system");
+                done();
+            });
         });
 
         // Test Other
         it("Other: must only have one route and if not then 404 error)", (done) => {
-            supertest(app)
-                .get("/invalid-route")
-                .expect(404)
-                .end((error, res) => {
-                    if (error)
-                        return done(error);
-                    done();
-                });
+            request.get('http://localhost:7865', (error, respose, body) => {
+                expect(respose.statusCode).to.equal(400);
+                done();
+            });
         });
     });
-
 });
