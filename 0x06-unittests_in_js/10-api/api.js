5c60209efe4570519efc4727545908953b07af44
jsump <libbyn79@gmail.com>
2024-05-03 22:32:36 +0300

    Task 10

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,57 @@
+/**
+ * This file cretes an instance that listens to a port
+ */
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// create an instance of express
+const app = express();
+
+//Middleware to parse JSON request body
+app.use(bodyParser.json());
+
+// Return message from GRT route
+app.get("/", (req, res) => {
+    res.send("Welcome to the payment system");
+});
+
+//Endpoint for cart
+app.get("/cart/:id(\\d+)", (req, res) => {
+    const id = req.params.id;
+
+    if (isNaN(id)) {
+        res.status(404).json({ error: "Invalid cart ID. Must be a number." });
+    } else {
+        res.json(`Payment methods for cart ${id}`);
+    }
+});
+
+
+// Endpoint to get available payments
+app.get('/available_payments', (req, res) => {
+    res.json({
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+          }
+    });
+});
+
+// Endpoint to welcome user
+app.post('/login', (req, res) => {
+    const userName = req.body.userName;
+    res.send(`Welcome ${userName}`);
+});
+
+
+// Listen and log
+app.listen(7865, () => {
+    console.log("API available on localhost port 7865");
+});
+
+// Error
+app.use((req, res) => {
+    res.status(404).send("404");
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,79 @@
+const request = require('request')
+const chai = require('chai');
+const { expect } = require("chai");
+
+const app = require("./api.js");
+
+
+describe("API", () => {
+    describe("GET /", () => {
+        // Test correct status code
+        it("must return the correct status code(OK: 200)", (done) => {
+            request.get('http://localhost:7865', (error, response, body) => {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        // Test correct result
+        it("must return the correct result", (done) => {
+            request.get('http://localhost:7865', (error, response, body) => {
+                expect(body).to.equal("Welcome to the payment system");
+                done();
+            });
+        });
+
+        // Test Other
+        it("Other: must only have one route and if not then 404 error)", (done) => {
+            request.get('http://localhost:7865/undefined-route', (error, response, body) => {
+                expect(response.statusCode).to.equal(404);
+                done();
+            });
+        });
+    });
+
+    describe("Cart Page", () => {
+        //Test correct status code when id is a number
+        it("must return correct status code when id is number", (done) => {
+            request.get('http://localhost:7865/cart/123', (error, response, body) => {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        // Test for correct status code when id is npt a number
+        it("must return correct status code when id is not a number", (done) => {
+            request.get('http://localhost:7865/cart/abc', (error, response, body) => {
+                expect(response.statusCode).to.equal(404);
+                done();
+            });
+        })
+    });
+
+    describe('Login endpoing', () => {
+        it("must return a welcome message withthe username", (done) => {
+            request.post('http://localhost:7865/login', { json: { userName: "John" } }, (error, response, body) => {
+                expect(response.statusCode).to.equal(200);
+                expect(response.body).to.equal("Welcome John");
+                done();
+            });
+
+        });
+    });
+
+    describe('Get available payment methods', () => {
+        it("must return an object with available payment methods", (done) => {
+            request.get('http://localhost:7865/available_payments', (error, response, body) => {
+                expect(response.statusCode).to.equal(200);
+                expect(response.body).to.eql({
+                    payment_methods: {
+                        credit_cards: true,
+                        paypal: false
+                    }
+                });
+                done();
+            });
+        });
+
+    });
+});
