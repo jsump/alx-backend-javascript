7c4fafa9b36ae7b0a24687e6cef4ad05b0e68c8e
jsump <libbyn79@gmail.com>
2024-05-03 15:17:03 +0300

    File and test

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,39 @@
+const { request } = require("express");
+
+describe("API", () => {
+    describe("GET /", () => {
+        it(" must return the correct status code(OK: 200)", (done) => {
+            request(app)
+                .get("/")
+                .expect(200)
+                .end((error, res) => {
+                    if (error)
+                        return done(error);
+                    done();
+                });
+        });
+
+        it("must return the correct result", (done) => {
+            request(app)
+                .get("/")
+                .expect("Welcome to the payment system")
+                .end((error, res) => {
+                    if (error)
+                        return done(error);
+                    done();
+                });
+        });
+
+        it("Other: must only have one route and if not then 404 error)", (done) => {
+            request(app)
+                .get("/invalid-route")
+                .expect("404")
+                .end((error, res) => {
+                    if (error)
+                        return done(error);
+                    done();
+                });
+        });
+    });
+
+});
