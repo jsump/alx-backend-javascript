e9b0feba9b9b255a0764557ba76b3af844eab6a2
jsump <libbyn79@gmail.com>
2024-05-03 13:18:36 +0300

    Revert "Task 2 edit"
    This reverts commit 8236ef2a226794e211751110476466b205d114b0.

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,32 +1,30 @@
-(async () => {
-	const { expect } = await import('chai');
-	const calculateNumber = require('./2-calcul_chai.js');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai.js');
 
 
-	describe('calculateNumber', () => {
-		describe('SUM', () => {
-			it('must correctly add two rounded numbers', () => {
-				const result = calculateNumber('SUM', 6.7, 4.6);
-				expect(result).to.equal(12);
-			});
-		});
-
-		describe('SUBTRACT', () => {
-			it('must correctly subtract two rounded numbers', () => {
-				const result = calculateNumber('SUBTRACT', 5, 2);
-				expect(result).to.equal(3);
-			});
-		});
-
-		describe('DIVIDE', () => {
-			it('must correctly divide two rounded numbers', () => {
-				const result = calculateNumber('DIVIDE', 6, 3);
-				expect(result).to.equal(2);
-			});
-			it('must return "Error" when dividing by zero', () => {
-				const result = calculateNumber('DIVIDE', 4, 0);
-				expect(result).to.equal('Error');
-			});
+describe('calculateNumber', () => {
+	describe('SUM', () => {
+		it('must correctly add two rounded numbers', () => {
+			const result = calculateNumber('SUM', 6.7, 4.6);
+			expect(result).to.equal(12);
 		});
 	});
-})();
+
+	describe('SUBTRACT', () => {
+		it('must correctly subtract two rounded numbers', () => {
+			const result = calculateNumber('SUBTRACT', 5, 2);
+			expect(result).to.equal(3);
+		});
+	});
+
+	describe('DIVIDE', () => {
+		it('must correctly divide two rounded numbers', () => {
+			const result = calculateNumber('DIVIDE', 6, 3);
+			expect(result).to.equal(2);
+		});
+		it('must return "Error" when dividing by zero', () => {
+			const result = calculateNumber('DIVIDE', 4, 0);
+			expect(result).to.equal('Error');
+		});
+	});
+});
