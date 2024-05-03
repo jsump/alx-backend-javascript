4a2852f4022171c6b068ed2e3e465657e8b52791
jsump <libbyn79@gmail.com>
2024-05-03 18:13:34 +0300

    Task 9

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,35 @@
+/**
+ * This file cretes an instance that listens to a port
+ */
+const express = require("express");
+
+// create an instance of express
+const app = express();
+
+// Return message from GRT route
+app.get("/", (req, res) => {
+    res.send("Welcome to the payment system");
+});
+
+//Endpoing for cart
+app.get("/cart/:id(\\d+)", (req, res) => {
+    const id = req.params.id;
+
+    if (isNaN(id)) {
+        res.status(400).json({ error: "Invalid cart ID. Must be a number." });
+    } else {
+        res.json({ message: `Payment methods for cart ${id}` });
+    }
+});
+
+// Listen and log
+app.listen(7865, () => {
+    console.log("API available on localhost port 7865");
+});
+
+// Error
+app.use((req, res) => {
+    res.status(404).send("404");
+});
+
+module.exports = app;
