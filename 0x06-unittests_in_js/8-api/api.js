22d6f67cc8e70497390d51eb0d85edf28f0bda48
jsump <libbyn79@gmail.com>
2024-05-03 17:05:42 +0300

    File and test

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,19 @@
+/**
+ * This file cretes an instance that listens to a port
+ */
+const express = require("express");
+
+// create an instance of express
+const app = express();
+
+// Return message from GRT route
+app.get("/", (req, res) => {
+    res.send("Welcome to the payment system");
+});
+
+// Listen and log
+app.listen(7865, () => {
+    console.log("API available on localhost port 7865");
+});
+
+module.exports = app;
