a82779e3028535b095aeeb04a9343d3f0c56e81c
jsump <libbyn79@gmail.com>
2024-05-03 14:22:52 +0300

    BeforeEach and AfterEach

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -7,41 +7,48 @@ const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./5-payment.js");
 
 describe("sendPaymentRequestToApi", () => {
-    it("must call Utils.calculateNumber stub with the correct args", () => {
+    let calculateNumberStub;
+    let consoleLogSpy
+
+    beforeEach(() = {
         // Stub Utils.calculateNumber to aleays return 120
-        const calculateNumberSpy = sinon.stub(Utils, "calculateNumber").returns(120);
+        calculateNumberStub = sinon.stub(Utils, "calculateNumber").returns(120);
         // Spy on console log to verify the output
-        const consoleLogSpy = sinon.spy(console, "log");
+        consoleLogSpy = sinon.spy(console, "log");
+    });
 
-        // call the fucntion
-        sendPaymentRequestToApi(100, 20);
-
-        // Verify that Utils.calculateNumber is called with correct args
-        sinon.assert.calledWithExactly(calculateNumberSpy, "SUM", 100, 20);
-        // //correct message
-        sinon.assert.calledWithExactly(consoleLogSpy, "The total is: 120");
-
+    afterEach(() => {
         // Restore stub and spy
-        calculateNumberSpy.restore();
+        calculateNumberStub.restore();
         consoleLogSpy.restore();
     });
 
     it("must call Utils.calculateNumber stub with the correct args", () => {
-        // Stub Utils.calculateNumber to aleays return 20
-        const calculateNumberSpy = sinon.stub(Utils, "calculateNumber").returns(20);
-        // Spy on console log to verify the output
-        const consoleLogSpy = sinon.spy(console, "log");
+        // call the fucntion
+        sendPaymentRequestToApi(100, 20);
+
+        // Verify that Utils.calculateNumber is called with correct args
+        sinon.assert.calledWithExactly(calculateNumberStub, "SUM", 100, 20);
+        // //correct message
+        sinon.assert.calledWithExactly(consoleLogSpy, "The total is: 120");
+
+
+    });
+
+    it("must call Utils.calculateNumber stub with the correct args", () => {
+        // Stub Utils.calculateNumber to aleays return 120
+        calculateNumberStub.returns(20);
 
         // call the fucntion
         sendPaymentRequestToApi(10, 10);
 
         // Verify that Utils.calculateNumber is called with correct args
-        sinon.assert.calledWithExactly(calculateNumberSpy, "SUM", 10, 10);
+        sinon.assert.calledWithExactly(calculateNumberStub, "SUM", 10, 10);
         // //correct message
         sinon.assert.calledWithExactly(consoleLogSpy, "The total is: 20");
 
         // Restore stub and spy
-        calculateNumberSpy.restore();
+        calculateNumberStub.restore();
         consoleLogSpy.restore();
     });
 });
