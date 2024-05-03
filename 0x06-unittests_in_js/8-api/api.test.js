e00270690192d93b0f86ed81560d78a20edebb31
jsump <libbyn79@gmail.com>
2024-05-03 17:10:11 +0300

    Revert "Edit"
    This reverts commit d3616f964fe55783c81e93425e135516ca29f36f.

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,41 +1,45 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const supertest = require("supertest");
 const app = require("./api.js");
+const { expect } = require("chai");
 
-// Configure chai
-chai.use(chaiHttp);
-const expect = chai.expect;
 
 describe("API", () => {
     describe("GET /", () => {
         // Test correct status code
-        it("must return the correct status code(OK: 200)", (done) => {
-            chai.request(app)
+        it(" must return the correct status code(OK: 200)", (done) => {
+            supertest(app)
                 .get("/")
+                .expect(200)
                 .end((error, res) => {
-                    expect(res).to.have.status(200);
+                    if (error)
+                        return done(error);
                     done();
                 });
         });
 
         // Test correct result
         it("must return the correct result", (done) => {
-            chai.request(app)
+            supertest(app)
                 .get("/")
+                .expect("Welcome to the payment system")
                 .end((error, res) => {
-                    expect(res.text).to.equal("Welcome to the payment system");
+                    if (error)
+                        return done(error);
                     done();
                 });
         });
 
         // Test Other
         it("Other: must only have one route and if not then 404 error)", (done) => {
-            chai.request(app)
+            supertest(app)
                 .get("/invalid-route")
+                .expect(404)
                 .end((error, res) => {
-                    expect(res).to.have.status(404);
+                    if (error)
+                        return done(error);
                     done();
                 });
         });
     });
+
 });
