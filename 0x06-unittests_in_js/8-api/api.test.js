67c27f22eecba197afd08ec9989dffc94b51f72b
jsump <libbyn79@gmail.com>
2024-05-05 23:19:19 +0300

    TEst

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,20 +1,24 @@
+const request = require('request')
 const chai = require('chai');
-const chaiHttp = require('chai-http');
 const { expect } = require("chai");
 
 const app = require("./api.js");
 
-chai.use(chaiHttp);
-
 describe("Index page", () => {
-    // Test correct status code and result
-    it("must return the correct status code and result", (done) => {
-        chai.request(app)
-            .get('/')
-            .end((error, response) => {
-                expect(response).to.have.status(200);
-                expect(response.text).to.equal("Welcome to the payment system");
-                done();
-            });
+    // Test correct status code
+    it("must return the correct status code(OK: 200)", (done) => {
+        request.get('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    // Test correct result
+    it("must return the correct result", (done) => {
+        request.get('http://localhost:7865', (error, response, body) => {
+            expect(body).to.equal("Welcome to the payment system");
+            done();
+        });
     });
 });
+
